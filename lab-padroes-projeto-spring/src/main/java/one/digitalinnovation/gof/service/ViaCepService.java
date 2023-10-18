a13bb8cccb10ed049541c40cef08f26f4ff141b5
Samuel Nascimento <sfsnascimento@gmail.com>
2023-10-17 22:20:56 -0300

    criando interface ViaCepService

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "http://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
